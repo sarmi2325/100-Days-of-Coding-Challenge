e8c058dece6bc3a685293643c16901810a1b9cfe
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2024-07-07 16:49:51 +0530

    Create Reverse Number.c

---
+++ Day 2/Reverse Number.c
@@ -0,0 +1,12 @@
+int reverse(int x){
+  int sum=0;
+  while(x>0){
+    int n=x%10;
+    x/=10;
+    if(sum>INT_MAX/10 || sum<INT_MIN/10){
+      return 0;
+    }
+    sum=sum*10+n;
+  }
+  return sum;
+}
