4f854662c9a15eb89ce70718be2529b55d4dee0d
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2024-07-19 10:51:02 +0530

    Create Perfect Number.c

---
+++ Day 5/Perfect Number.c
@@ -0,0 +1,12 @@
+bool checkPerfectNumber(int num) {
+    int sum=0;
+    for(int i=1;i<=num/2;i++){
+        if(num%i==0){
+            sum=sum+i;
+        }
+    }
+    if(sum==num){
+        return 1;
+    }
+    return 0;
+}
